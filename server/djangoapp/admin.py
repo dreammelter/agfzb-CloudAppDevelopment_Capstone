19cd1a1017ebc276cf9dc29496c8047283a68efc
dreammelter <dreammelter@users.noreply.github.com>
2021-07-20 20:38:18 -0700

    added car models, app settings, and reviewer acct - close #13

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
-# CarModelInline class
+class CarModelInline:
+    model = CarModel
 
-# CarModelAdmin class
 
-# CarMakeAdmin class with CarModelInline
+class CarModelAdmin:
+    list_display = ['name']
+
+
+class CarMakeAdmin:
+    inlines = [CarModelInline]
+    list_display = ('name', 'car_year')
+    list_filter = ['car_type']
+    search_fields = ['name','car_year','car_type']
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
