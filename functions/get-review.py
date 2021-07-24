1f8f1a6596d8c244fede2de43d8049847737bf81
dreammelter <dreammelter@users.noreply.github.com>
2021-07-23 21:59:38 -0700

    added notes about file usage...

--- functions/get-review.py
+++ functions/get-review.py
@@ -1,12 +1,7 @@
-#
-#
-# main() will be run when you invoke this action
-#
-# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-#
-# @return The output of this action, which must be a JSON object.
-#
-#
+"""
+I did not use this file at all, since I repurposed the existing functions...
+So you may want to check out the pass-the-docs.js and format-docs.js files.
+"""
 import sys
 
 def main(dict):
